dbdcda8069050f4f86c249b53af8cee85b73cdcb
Omar <omarkiojoo@gmail.com>
2022-11-05 13:19:21 +0100

    hardhat setup

---
+++ client/src/page/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className="text-5xl p-3">Hour Zero</h1>
+      <h2 className="text-3xl p-3">
+        Choose Your Champions, Make Your Move, And Be A Hero In The Justice League NFT Strategy Card Game.
+      </h2>
+      <p className="text-xl p-3">Made with 💜 by JavaScript Mastery</p>
+    </div>
+  )
+}
+
+export default Home
